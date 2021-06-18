e6761d61e1e98b926ea3f6d32dad671bdf8b2a0c
Victor Basso <vitobasso@gmail.com>
2021-06-18 18:51:50 -0300

    use io::start

--- src/gui.rs
+++ src/gui.rs
@@ -1,4 +1,4 @@
-use piston_window::{OpenGL, PistonWindow, WindowSettings, Event::*};
+use piston_window::{OpenGL, PistonWindow, WindowSettings, Event::*, Input};
 use crate::keymap;
 use std::sync::mpsc::Sender;
 use rust_synth::core::control::manual_controller::Command;
@@ -6,7 +6,7 @@ use rust_synth::core::control::manual_controller::Command;
 const TITLE: &str = "Sintetizador Maravilhoso";
 const WINDOW_SIZE: [f64;2] = [800., 800.];
 
-pub fn render(synth_commands: Sender<Command>) {
+pub fn start(synth_commands: Option<Sender<Command>>) {
     let opengl = OpenGL::V3_2;
 
     let mut window: PistonWindow = WindowSettings::new(TITLE, WINDOW_SIZE)
@@ -17,12 +17,18 @@ pub fn render(synth_commands: Sender<Command>) {
     while let Some(e) = window.next() {
         match &e {
             Input(input) => {
-                let commands = keymap::handle_input(input, WINDOW_SIZE);
-                for command in commands {
-                    synth_commands.send(command).expect("Failed to send synth command")
-                }
+                handle_input(&synth_commands, input)
             },
             _ => (),
+        }
+    }
+}
+
+fn handle_input(synth_commands: &Option<Sender<Command>>, input: &Input) {
+    if let Some(channel) = &synth_commands {
+        let commands = keymap::handle_input(input, WINDOW_SIZE);
+        for command in commands {
+            channel.send(command).expect("Failed to send synth command")
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,24 @@
-use std::{thread, sync::mpsc::{channel, sync_channel}};
-use rust_synth::core::{synth::Sample, control::{manual_controller::{self, Command}, playback_controller}};
-use rust_synth::io::{audio, midi};
-use rust_synth::preset;
+use std::env::{args, Args};
+use rust_synth::io;
 
 mod gui;
 mod keymap;
 
 fn main() {
-    let out = audio::Out::initialize().unwrap_or_else(|e| panic!("{}", e));
-    let sample_rate = out.sample_rate();
-
-    let (command_out, command_in) = channel::<Command>();
-    let (sound_out, sound_in) = sync_channel::<Sample>(out.buffer_size());
-
-    thread::spawn(move || out.loop_forever(sound_in));
-    match read_midi_file() {
-        Some(song) => thread::spawn(move || playback_controller::loop_forever(sample_rate, song, sound_out)),
-        None       => thread::spawn(move || manual_controller::loop_forever(sample_rate, preset::patches(), command_in, sound_out)),
-    };
-
-    gui::render(command_out);
+    match midi_file_argument() {
+        Some(file_name) => {
+            io::start_midi(&file_name);
+            gui::start(None);
+        },
+        None => {
+            let commands_out = io::start_manual();
+            gui::start(Some(commands_out));
+        },
+    }
 }
 
-use std::env::{args,Args};
-use rust_synth::core::control::song::Song;
-fn read_midi_file() -> Option<Song> {
+fn midi_file_argument() -> Option<String> {
     let mut args: Args = args();
     args.next();
-    args.next().and_then(|file_name| midi::read_file(file_name.as_str()))
+    args.next()
 }
