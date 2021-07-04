11ea3539d3107936427f1af6a96e526f0d51b2bd
Victor Basso <vitobasso@gmail.com>
2021-07-04 12:52:05 -0300

    refactor

--- src/rendering.rs
+++ src/rendering.rs
@@ -1,7 +1,11 @@
-use piston_window::{PistonWindow, Event, Context, G2d, clear, text, Glyphs, Transformed};
-use rust_synth::core::control::tools::View;
+use std::collections::HashMap;
+use piston_window::{PistonWindow, Event, Context, G2d, clear, text, rectangle, Glyphs, Transformed};
 use piston_window::math::Scalar;
-use piston_window::rectangle;
+use rust_synth::core::control::tools::View;
+use rust_synth::core::control::synth::Id;
+use rust_synth::core::synth::filter;
+use rust_synth::core::tools::{arpeggiator, transposer};
+use rust_synth::core::music_theory::pitch::Pitch;
 
 pub type Color = [f32; 4];
 const BLACK: Color = [0.0, 0.0, 0.0, 1.0];
@@ -14,26 +18,11 @@ pub fn draw(view: View, window: &mut PistonWindow, glyphs: &mut Glyphs, e: &Even
         let patch = format!("patch: {}", view.selected_patch.to_string());
         draw_text(patch.as_str(), 10., 40., glyphs, c, g);
 
-        draw_text("cutoff:", 10., 60., glyphs, c, g);
-        draw_meter(view.synth.instrument.filter.cutoff, 100., 60., c, g);
-        draw_text("resonance:", 120., 60., glyphs, c, g);
-        draw_meter(view.synth.instrument.filter.resonance, 240., 60., c, g);
-
-
+        draw_filter(view.synth.instrument.filter, 10., 60., glyphs, c, g);
 
         if let Some(arp) = view.arpeggiator {
-            //arp.phrase.
-            draw_text("index:", 10., 160., glyphs, c, g);
-            draw_meter(view.arp_index, 100., 160., c, g);
-
-            let holding = arp.holding_pitch.map(|p| format!("holding: {}", p));
-            let playing = arp.playing_pitch.map(|p| format!("playing: {}", p));
-            if let (Some(holding), Some(playing)) = (holding, playing) {
-                let notes = format!("{}, {}", holding, playing);
-                draw_text(notes.as_str(), 10., 180., glyphs, c, g);
-            }
+            draw_arpeggiator(arp, view.arp_index, 10., 160., glyphs, c, g);
         }
-
 
         let pulse = format!("pulse: {}", view.pulse.period.as_millis().to_string());
         draw_text(pulse.as_str(), 10., 220., glyphs, c, g);
@@ -41,21 +30,43 @@ pub fn draw(view: View, window: &mut PistonWindow, glyphs: &mut Glyphs, e: &Even
         let loops = format!("{:?}", view.loops);
         draw_text(loops.as_str(), 10., 240., glyphs, c, g);
 
-
-
-
-        let transposer = format!("key: {}, pitch shift: {}", view.transposer.transposed_key,
-                                 view.transposer.pitch_shift);
-        draw_text(transposer.as_str(), 10., 740., glyphs, c, g);
-
-        let notes_vec = view.synth.holding_notes.values()
-            .map(|pitch| format!("{}", pitch))
-            .collect::<Vec<_>>();
-        let notes_str = format!("notes: {}", notes_vec.join(", "));
-        draw_text(notes_str.as_str(), 10., 760., glyphs, c, g);
-
-
+        draw_transposer(view.transposer, 10., 740., glyphs, c, g);
+        draw_notes(view.synth.holding_notes, 10., 760., glyphs, c, g);
     });
+}
+
+pub fn draw_filter(view: filter::View, x: Scalar, y: Scalar, glyphs: &mut Glyphs, c: Context, g: &mut G2d) {
+    draw_text("cutoff:", x, y, glyphs, c, g);
+    draw_meter(view.cutoff, x + 100., y, c, g);
+    draw_text("resonance:", x + 120., y, glyphs, c, g);
+    draw_meter(view.resonance, x + 240., y, c, g);
+}
+
+fn draw_arpeggiator(view: arpeggiator::View, index: f64, x: Scalar, y: Scalar, glyphs: &mut Glyphs, c: Context, g: &mut G2d) {
+    //arp.phrase.
+    draw_text("index:", x, y, glyphs, c, g);
+    draw_meter(index, x + 100., y, c, g);
+
+    let holding = view.holding_pitch.map(|p| format!("holding: {}", p));
+    let playing = view.playing_pitch.map(|p| format!("playing: {}", p));
+    if let (Some(holding), Some(playing)) = (holding, playing) {
+        let notes = format!("{}, {}", holding, playing);
+        draw_text(notes.as_str(), x, y + 20., glyphs, c, g);
+    }
+}
+
+fn draw_transposer(view: transposer::State, x: Scalar, y: Scalar, glyphs: &mut Glyphs, c: Context, g: &mut G2d) {
+    let transposer = format!("key: {}, pitch shift: {}", view.transposed_key,
+                             view.pitch_shift);
+    draw_text(transposer.as_str(), x, y, glyphs, c, g);
+}
+
+fn draw_notes(view: HashMap<Id, Pitch>, x: Scalar, y: Scalar, glyphs: &mut Glyphs, c: Context, g: &mut G2d) {
+    let notes_vec = view.values()
+        .map(|pitch| format!("{}", pitch))
+        .collect::<Vec<_>>();
+    let notes_str = format!("notes: {}", notes_vec.join(", "));
+    draw_text(notes_str.as_str(), x, y, glyphs, c, g);
 }
 
 pub fn draw_text(text: &str, x: Scalar, y: Scalar, glyphs: &mut Glyphs, c: Context, g: &mut G2d) {
@@ -66,6 +77,6 @@ pub fn draw_text(text: &str, x: Scalar, y: Scalar, glyphs: &mut Glyphs, c: Conte
 
 pub fn draw_meter(value: f64, x: Scalar, y: Scalar, c: Context, g: &mut G2d) {
     let c2 = c.trans(x, y);
-    let rect = rectangle::centered_square(0., -value * 20., 2.);
+    let rect = rectangle::centered_square(0., -value * 14., 2.);
     rectangle(WHITE, rect, c2.transform, g);
 }
